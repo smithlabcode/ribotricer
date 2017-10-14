d7dab9df131f083aee55be05e69096d65d81c474
Saket Choudhary <saketkc@gmail.com>
2017-10-14 11:56:06 -0700

    Add uniform signal simulator

--- riboraptor/helpers.py
+++ riboraptor/helpers.py
@@ -1,6 +1,20 @@
 from scipy import stats
 import numpy as np
 
+
+def create_uniform_signal(signal_length):
+    uniform_signal = np.array([4 / 6.0] * signal_length)
+    uniform_signal[range(1, len(uniform_signal), 3)] = 1 / 6.0
+    uniform_signal[range(2, len(uniform_signal), 3)] = 1 / 6.0
+    return uniform_signal
+
+
+def set_rotation(ax):
+    labels = ax.get_xticklabels()
+    for i in labels:
+        i.set_rotation(45)
+
+
 def round_to_nearest(x, base=5):
     '''Round to nearest base
 
@@ -16,6 +30,7 @@ def round_to_nearest(x, base=5):
     '''
     return int(base * round(float(x)/base))
 
+
 def r2(x, y):
     '''Calculate pearson correlation between two vectors
 
@@ -28,6 +43,7 @@ def r2(x, y):
     '''
     return stats.pearsonr(x, y)[0] ** 2
 
+
 def identify_peaks(coverage):
     """Given coverage array, find the site of maximum density"""
-    return np.argmax(coverage[range(-20, 20)])
+    return np.argmax(coverage[range(-30, 0)])
