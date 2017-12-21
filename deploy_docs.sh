1a4a0120c6388233a0c4d3f3094e4896789480b3
Saket Choudhary <saketkc@gmail.com>
2017-12-21 12:09:02 -0800

    Remove commiting to master branch from deploy docs

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 make clean
 make docs
 # commit and push
-git add riboraptor docs
-git commit -m "building and pushing docs"
-git push origin master
+#git add riboraptor docs
+#git commit -m "building and pushing docs"
+#git push origin master
 # switch branches and pull the data we want
 git checkout gh-pages
 rm -rf .
