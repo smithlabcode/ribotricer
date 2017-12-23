be83e503cf16fd6444adf0f6b836487e163e35c7
Saket Choudhary <saketkc@gmail.com>
2017-12-23 01:03:29 -0800

    More extensions for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,11 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram']
 
+extensions += [
+    'matplotlib.sphinxext.only_directives',
+    'matplotlib.sphinxext.plot_directive',
+    'matplotlib.sphinxext.ipython_directive',
+    'matplotlib.sphinxext.ipython_console_highlighting']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
