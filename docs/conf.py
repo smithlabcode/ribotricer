f4015ec70c1ef46c4762224f7f0e4951f146d981
Saket Choudhary <saketkc@gmail.com>
2017-12-29 23:34:44 -0800

    Try guzzle theme

--- docs/conf.py
+++ docs/conf.py
@@ -285,3 +285,17 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 numpydoc_show_class_members = False
+
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "ribocop",
+}
