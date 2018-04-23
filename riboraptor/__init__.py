3fab0508016d128a599b6dd35cf0eaad3fb5db2d
Saket Choudhary <saketkc@gmail.com>
2018-04-23 02:59:12 -0700

    Bump version: 0.2.0 → 0.2.1

--- riboraptor/__init__.py
+++ riboraptor/__init__.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 __author__ = """Saket Choudhary"""
 __email__ = 'saketkc@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .count import *
 from .wig import *
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requirements = requirements + ['pytest']
 
 setup(
     name='riboraptor',
-    version='0.2.0',
+    version='0.2.1',
     description="Python package to analyse ribosome profiling data",
     long_description=readme + '\n\n' + history,
     author="Saket Choudhary",
