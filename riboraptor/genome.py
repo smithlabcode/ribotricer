9ad1b7e8fe8bf0290a0ee11a3e1f3c5ea7371e99
Saket Choudhary <saketkc@gmail.com>
2018-04-26 12:07:32 -0700

    Fix annotation path

--- riboraptor/genome.py
+++ riboraptor/genome.py
@@ -18,14 +18,14 @@ __TAXON_ID_MAP__ = {
 def _get_bed(bedname, genome='hg38'):
     """Load bed from annotation.
     """
-    annotation_dir = os.path.join(
-        os.path.dirname(__file__), 'annotation', genome)
+    annotation_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'annotation', genome))
     annotation_file = os.path.join(annotation_dir, bedname + '.bed.gz')
     return annotation_file
 
 
 def _get_sizes(genome):
     """Load genome sizes file"""
-    annotation_dir = os.path.join(
-        os.path.dirname(__file__), 'annotation', genome)
+    annotation_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'annotation', genome))
     return os.path.join(annotation_dir, '{}.chrom.sizes'.format(genome))
