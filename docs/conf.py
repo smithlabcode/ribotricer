98822c4f817aeb93b0c52538a40d16336758b372
Saket Choudhary <saketkc@gmail.com>
2018-05-01 00:20:33 -0700

    mock out c modules from docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,16 @@
 
 import sys
 import os
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['pyBigWig', 'numpy', 'pandas', 'scipy', 'matplotlib', 'biopython', 'statsmodels']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
