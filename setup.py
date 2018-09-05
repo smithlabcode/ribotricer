d8534c36c3169691d9805fc28bcc3d3fa77751fc
Wenzheng Li <wenzhenl@usc.edu>
2018-09-05 15:38:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,51 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+import setuptools
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-with open('requirements.txt') as reqs:
-    requirements = reqs.readlines()
-
-test_requirements = requirements + ['pytest']
-
-setup(
-    name='riboraptor',
-    version='0.2.2',
-    description="Python package to analyse ribosome profiling data",
-    long_description=readme + '\n\n' + history,
-    author="Saket Choudhary",
-    author_email='saketkc@gmail.com',
-    url='https://github.com/saketkc/riboraptor',
-    packages=[
-        'riboraptor',
-    ],
-    package_dir={'riboraptor': 'riboraptor'},
-    package_data={
-        'riboraptor':
-        ['annotation/hg38/*.*', 'annotation/mm10/*.*',
-        'annotation/sacCerR64/*.*', 'tests/data/*.*']
-    },
-    #data_files=[('riboraptor', ['annotation/*.*'])],
-    entry_points={'console_scripts': ['riboraptor=riboraptor.cli:cli']},
-    scripts=['scripts/download_sra_data', 'scripts/convert_gse_to_srp'],
-    include_package_data=True,
-    install_requires=requirements,
-    license="BSD license",
-    zip_safe=False,
-    keywords='riboraptor',
+setuptools.setup(
+    name='RiboCop',
+    version='0.2.7',
+    author='Wenzheng Li',
+    author_email='wenzhenl@usc.edu',
+    description="Python package to detect translating ORF from Ribo-seq data",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wenzhenl/ribocop',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['RiboCop=RiboCop.cli:cli']},
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements)
+)
