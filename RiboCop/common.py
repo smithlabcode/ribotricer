3dd50a971fb2e9cabba27bb58994c94c9dae501f
Wenzheng Li <wenzhenl@usc.edu>
2018-09-12 12:03:23 -0700

    added merge_intervals

--- RiboCop/common.py
+++ RiboCop/common.py
@@ -1,3 +1,6 @@
+from .interval import Interval
+
+
 def is_read_uniq_mapping(read):
     """Check if read is uniquely mappable.
 
@@ -28,3 +31,29 @@ def is_read_uniq_mapping(read):
     if nh_count == 1:
         return True
     return False
+
+
+def merge_intervals(intervals):
+    """
+    Parameters
+    ----------
+    intervals: List[Interval]
+
+    Returns
+    -------
+    merged_intervals: List[Interval]
+                      sorted and merged intervals
+    """
+
+    sorted_intervals = sorted(intervals, key=lambda i: i.start)
+    merged_intervals = []
+    i = 0
+    while i < len(sorted_intervals):
+        to_merge = Interval(sorted_intervals[i].start, sorted_intervals[i].end)
+        while i + 1 < len(sorted_intervals
+                          ) and sorted_intervals[i + 1].start <= to_merge.end:
+            to_merge.end = max(to_merge.end, sorted_intervals[i + 1].end)
+            i += 1
+        merged_intervals.append(to_merge)
+        i += 1
+    return merged_intervals
