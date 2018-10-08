403804ac49903e67d0f2e38d9cbc6a4bdf423954
Wenzheng Li <wenzhenl@usc.edu>
2018-10-08 16:55:13 -0700

    added coherence and wilcoxon test

--- RiboCop/common.py
+++ RiboCop/common.py
@@ -2,29 +2,86 @@
 """
 import numpy as np
 from .interval import Interval
+from .test_func import wilcoxon_greater, combine_pvals
 from scipy import stats
+from scipy import signal
 
 
 def cal_periodicity(values):
-    repeats = len(values) // 3
-    total = repeats * 3
-    values = values[:total]
-    corr, pval = 0, 1.0
-    frame0 = np.array([1, 0, 0] * repeats)
-    r, p = stats.pearsonr(values, frame0)
-    if abs(r) > corr:
-        corr, pval = abs(r), p
+    coh = coherence(values)
+    pval = wilcoxon(values)
+    return (coh, pval)
 
-    frame1 = np.array([0, 1, 0] * repeats)
-    r, p = stats.pearsonr(values, frame1)
-    if abs(r) > corr:
-        corr, pval = abs(r), p
 
-    frame2 = np.array([0, 0, 1] * repeats)
-    r, p = stats.pearsonr(values, frame2)
-    if abs(r) > corr:
-        corr, pval = abs(r), p
-    return (corr, pval)
+def wilcoxon(values):
+    length = len(values) // 3 * 3
+    values = values[:length]
+    f0 = values[0:length:3]
+    f1 = values[1:length:3]
+    f2 = values[2:length:3]
+    final_pv1=final_pv2=final_pv=1.0
+
+    pv1 = wilcoxon_greater(f0, f1)
+    pv2 = wilcoxon_greater(f0, f2)
+    pv = combine_pvals(np.array([pv1.pvalue, pv2.pvalue]))
+    if pv < final_pv:
+        final_pv = pv
+        final_pv1 = pv1
+        final_pv2 = pv2
+
+    pv1 = wilcoxon_greater(f1, f0)
+    pv2 = wilcoxon_greater(f1, f2)
+    pv = combine_pvals(np.array([pv1.pvalue, pv2.pvalue]))
+    if pv < final_pv:
+        final_pv = pv
+        final_pv1 = pv1
+        final_pv2 = pv2
+
+    pv1 = wilcoxon_greater(f2, f0)
+    pv2 = wilcoxon_greater(f2, f1)
+    pv = combine_pvals(np.array([pv1.pvalue, pv2.pvalue]))
+    if pv < final_pv:
+        final_pv = pv
+        final_pv1 = pv1
+        final_pv2 = pv2
+    return final_pv
+
+def coherence(values):
+    """Calculate coherence and an idea ribo-seq signal
+
+    Parameters
+    ----------
+    values : array like
+             List of values
+
+    Returns
+    -------
+    periodicity : float
+                  Periodicity score calculated as
+                  coherence between input and idea 1-0-0 signal
+
+    f: array like
+       List of frequencies
+
+    Cxy: array like
+         List of coherence at the above frequencies
+
+    """
+    length = len(values) // 3 * 3
+    values = values[:length]
+    uniform_signal = [0.7, 0.2, 0.1] * (length // 3)
+    mean_centered_values = values - np.nanmean(values)
+    normalized_values = mean_centered_values / \
+        np.max(np.abs(mean_centered_values))
+
+    mean_centered_values = uniform_signal - np.nanmean(uniform_signal)
+    uniform_signal = mean_centered_values / \
+        np.max(np.abs(uniform_signal))
+    f, Cxy = signal.coherence(
+        normalized_values, uniform_signal, nperseg=30, noverlap=27)
+    periodicity_score = Cxy[np.argwhere(np.isclose(f, 1 / 3.0))[0]][0]
+    # return periodicity_score, f, Cxy
+    return periodicity
 
 
 def is_read_uniq_mapping(read):
