aa286da54d9a68214ef24b20eb3018692f8d70d8
Wenzheng Li <wenzhenl@usc.edu>
2018-12-18 14:50:03 -0800

    small changes

--- RiboCop/cli.py
+++ RiboCop/cli.py
@@ -1,13 +1,10 @@
-"""Command line interface for riboraptor
+"""Command line interface for RiboCop
 """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
-import sys
-from textwrap import dedent
 import numpy as np
 import pandas as pd
 
