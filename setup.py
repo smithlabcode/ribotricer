1b0c5da1b4d406be3717765e1a0bf4b1a2f8fce0
Saket Choudhary <saketkc@gmail.com>
2019-03-15 02:46:40 -0700

    Bump version: 0.2.7 → 0.3.0-dev0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as req_file:
 
 setuptools.setup(
     name='ribotricer',
-    version='0.2.7',
+    version='0.3.0-dev0',
     author='Wenzheng Li, Saket Choudhary',
     author_email='wenzhenl@usc.edu',
     description="Python package to detect translating ORF from Ribo-seq data",
