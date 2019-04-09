344932d848c7955c533532db28e8eaac5118d2e4
Wenzheng Li <wenzhenl@usc.edu>
2019-04-09 13:17:36 -0700

    added copyright info to each executable file

--- ribotricer/const.py
+++ ribotricer/const.py
@@ -1,4 +1,18 @@
 """constants used in this program"""
+# Part of ribotricer software
+#
+# Copyright (C) 2019 Wenzheng Li, Saket Choudhary and Andrew D Smith
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 CUTOFF = 0.428571428571
 TYPICAL_OFFSET = 12
 MINIMUM_VALID_CODONS = 5
--- ribotricer/gtf.py
+++ ribotricer/gtf.py
@@ -1,5 +1,18 @@
 """Utilities for reading GTF file
 """
+# Part of ribotricer software
+#
+# Copyright (C) 2019 Wenzheng Li, Saket Choudhary and Andrew D Smith
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 from collections import defaultdict
 from tqdm import *
