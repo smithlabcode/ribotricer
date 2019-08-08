d763cba895ad4cc2b43013759a1e4ceaefdd6738
Saket Choudhary <saketkc@gmail.com>
2019-08-08 02:29:02 -0700

    Add orfs-seq for for getting the fasta sequence

---
+++ ribotricer/orf_seq.py
@@ -0,0 +1,47 @@
+"""Generate sequences for ribotricer annotation"""
+
+import pandas as pd
+import pyfaidx
+
+from tqdm import tqdm
+
+
+from .interval import Interval
+from .fasta import FastaReader
+
+
+def orf_seq(ribotricer_index, genome_fasta, saveto):
+    """Generate sequence for ribotricer annotation.
+  
+  Parameters
+  -----------
+
+  ribotricer_index: string
+                         Path to ribotricer generate annotation 
+  genome_Fasta: string
+                Path to genome fasta
+
+  saveto: string
+          Path to output
+  """
+    fasta = FastaReader(genome_fasta)
+    annotation_df = pd.read_csv(ribotricer_index, sep="\t")
+    with open(saveto, "w") as fh:
+        for idx, row in tqdm(annotation_df.iterrows(), total=annotation_df.shape[0]):
+            chrom = row.chrom
+            orf_id = row.ORF_ID
+            coordinates = row.coordinate.split(",")
+            strand = row.strand
+            intervals = []
+            seq = ""
+            for coordinate in coordinates:
+                start, stop = coordinate.split("-")
+                start = int(start)
+                stop = int(stop)
+                interval = Interval(chrom, start, stop, strand)
+                intervals.append(interval)
+
+            seq = (" ").join(fasta.query(intervals))
+            if strand == "-":
+                seq = fasta.reverse_complement(seq)
+            fh.write("{}\t{}".format(orf_id, seq))
