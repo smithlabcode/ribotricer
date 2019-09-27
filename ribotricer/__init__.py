7b16d0b4f16ffdc06d6fa692ab681ae939b2439b
Saket Choudhary <saketkc@gmail.com>
2019-09-27 11:38:52 -0700

    Bump version: 1.1.0-dev0 → 1.1.0

--- ribotricer/__init__.py
+++ ribotricer/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = "Wenzheng Li, Saket Choudhary"
-__version__ = "__version__ = '1.1.0-dev0'"
+__version__ = "__version__ = '1.1.0'"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt") as req_file:
 
 setuptools.setup(
     name="ribotricer",
-    version="version='1.1.0-dev0'",
+    version="version='1.1.0'",
     author="Wenzheng Li, Saket Choudhary",
     author_email="skchoudh@usc.edu",
     description="Python package to detect translating ORF from Ribo-seq data",
