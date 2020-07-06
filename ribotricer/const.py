a95bc9671db3753888c979a4e15e4397236551bc
Saket Choudhary <saketkc@gmail.com>
2020-07-06 13:24:09 -0700

    Changed minimum valid codons to float

--- ribotricer/const.py
+++ ribotricer/const.py
@@ -25,7 +25,7 @@ MINIMUM_VALID_CODONS = 5
 # default: 0 (decided by CUTOFF and MINIMUM_VALID_CODONS)
 MINIMUM_READS_PER_CODON = 0
 # fraction of codons with non zero reads
-MINIMUM_VALID_CODONS_RATIO = 0
+MINIMUM_VALID_CODONS_RATIO = 0.0
 # Minimum read density over ORF
 # defined as the number of reads per unit length of the ORF
 MINIMUM_DENSITY_OVER_ORF = 0.0
